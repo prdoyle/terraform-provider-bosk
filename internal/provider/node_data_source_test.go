1aecd8f48dd07f66f62ba98a756c4601bdfab5f0
Patrick Doyle <pdoyle@venacorp.com>
2023-11-06 11:42:14 -0500

    Use mock HTTP server for acceptance tests

--- internal/provider/node_data_source_test.go
+++ internal/provider/node_data_source_test.go
@@ -4,31 +4,42 @@
 package provider
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccNodeDataSource(t *testing.T) {
+	// Thank you https://github.com/hashicorp/terraform-provider-http/blob/main/internal/provider/data_source_http_test.go
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, err := w.Write([]byte("[{\"world\":{\"id\":\"world\"}}]"))
+		if err != nil {
+			t.Errorf("error writing body: %s", err)
+		}
+	}))
+	defer testServer.Close()
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: testAccNodeDataSourceConfig,
+				Config: fmt.Sprintf(`
+					data "bosk_node" "test" {
+						url        = "%s"
+						value_json = "[]"
+					}
+				`, testServer.URL),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.bosk_node.test", "url", "http://localhost:1740/bosk/targets"),
+					resource.TestCheckResourceAttr("data.bosk_node.test", "url", testServer.URL),
 					resource.TestCheckResourceAttr("data.bosk_node.test", "value_json", "[{\"world\":{\"id\":\"world\"}}]"),
 				),
 			},
 		},
 	})
 }
-
-const testAccNodeDataSourceConfig = `
-data "bosk_node" "test" {
-  url = "http://localhost:1740/bosk/targets"
-  value_json = "[]"
-}
-`
